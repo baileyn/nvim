680b01464f5f2c8bf44754cd6789a736c3356e07
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:55:14 -0400

    feat(plugins): fix commenting for tsx

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -188,9 +188,44 @@ local function init()
 	})
 
 	use({
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		require("nvim-treesitter.configs").setup({
+			context_commentstring = {
+				enable = true,
+			},
+		}),
+	})
+	use({
 		"numToStr/Comment.nvim",
 		config = function()
-			require("Comment").setup()
+			require("Comment").setup(
+				-- NOTE: The example below is a proper integration and it is RECOMMENDED.
+				{
+					---@param ctx Ctx
+					pre_hook = function(ctx)
+						-- Only calculate commentstring for tsx filetypes
+						if vim.bo.filetype == "typescriptreact" then
+							local U = require("Comment.utils")
+
+							-- Detemine whether to use linewise or blockwise commentstring
+							local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+
+							-- Determine the location where to calculate commentstring from
+							local location = nil
+							if ctx.ctype == U.ctype.block then
+								location = require("ts_context_commentstring.utils").get_cursor_location()
+							elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+								location = require("ts_context_commentstring.utils").get_visual_start_location()
+							end
+
+							return require("ts_context_commentstring.internal").calculate_commentstring({
+								key = type,
+								location = location,
+							})
+						end
+					end,
+				}
+			)
 		end,
 	})
 
@@ -242,13 +277,16 @@ local function init()
 			})
 		end,
 	})
-    use({'jose-elias-alvarez/null-ls.nvim', config = function()
-        require("null-ls").setup({
-            sources = {
-                require("null-ls").builtins.formatting.stylua,
-            }
-        })
-    end})
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("null-ls").setup({
+				sources = {
+					require("null-ls").builtins.formatting.stylua,
+				},
+			})
+		end,
+	})
 end
 
 local plugins = setmetatable({}, {
