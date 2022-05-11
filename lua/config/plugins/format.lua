3b31cb8078ed96a6b81c2adc65e0a6a7aabf322b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-05-10 20:55:07 -0400

    feat(format): add formatter for typescriptreact

--- lua/config/plugins/format.lua
+++ lua/config/plugins/format.lua
@@ -24,6 +24,16 @@ require('formatter').setup({
 				}
 			end,
 		},
+        typescriptreact = {
+			-- prettier
+			function()
+				return {
+					exe = 'prettier',
+					args = { '--stdin-filepath', vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote' },
+					stdin = true,
+				}
+			end,
+        },
 		javascript = {
 			-- prettier
 			function()
