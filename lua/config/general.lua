8de09d5f8438ecb153b02ef492e8cb112b6ff86b
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-08-01 11:35:52 -0600

    chore: format some files

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -21,48 +21,48 @@ vim.wo.relativenumber = true
 
 vim.api.nvim_create_augroup("line_numbers", {})
 vim.api.nvim_create_autocmd("InsertEnter", {
-	group = "line_numbers",
-	command = ":set norelativenumber",
+    group = "line_numbers",
+    command = ":set norelativenumber",
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-	group = "line_numbers",
-	command = ":set relativenumber",
+    group = "line_numbers",
+    command = ":set relativenumber",
 })
 
 vim.api.nvim_create_augroup("formatting", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = "formatting",
-	callback = function()
-		vim.lsp.buf.formatting_seq_sync()
-	end,
+    group = "formatting",
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
 
 function OrgImports(wait_ms)
-	local params = vim.lsp.util.make_range_params()
-	params.context = { only = { "source.organizeImports" } }
-	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-	for _, res in pairs(result or {}) do
-		for _, r in pairs(res.result or {}) do
-			if r.edit then
-				vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-			else
-				vim.lsp.buf.execute_command(r.command)
-			end
-		end
-	end
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
 end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = "formatting",
-	pattern = "*.go",
-	callback = function()
-		OrgImports(1000)
-	end,
+    group = "formatting",
+    pattern = "*.go",
+    callback = function()
+        OrgImports(1000)
+    end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-	group = "line_numbers",
-	command = ":set relativenumber",
+    group = "line_numbers",
+    command = ":set relativenumber",
 })
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
--- lua/config/plugins/neorg.lua
+++ lua/config/plugins/neorg.lua
@@ -1,37 +1,41 @@
 require("neorg").setup({
-	load = {
-		["core.defaults"] = {},
-		["core.keybinds"] = {
-			config = {
-				neorg_leader = " ",
-			},
-		},
-		["core.gtd.base"] = {
-			config = {
-				workspace = "home",
-			},
-		},
+    load = {
+        ["core.defaults"] = {},
+        ["core.keybinds"] = {
+            config = {
+                neorg_leader = " ",
+            },
+        },
+        ["core.export"] = {},
+        ["core.export.markdown"] = {},
+        ["core.norg.journal"] = {},
+        ["core.gtd.base"] = {
+            config = {
+                workspace = "home",
+            },
+        },
         ["core.gtd.ui"] = {},
         ["core.gtd.helpers"] = {},
-		["core.presenter"] = {
-			config = {
-				zen_mode = "zen-mode",
-			},
-		},
-		["core.norg.completion"] = {
-			config = {
-				engine = "nvim-cmp",
-			},
-		},
-		["core.norg.concealer"] = {},
-		["core.norg.dirman"] = {
-			config = {
-				workspaces = {
-					home = "~/notes/home",
-				},
-			},
-		},
-		["core.norg.qol.toc"] = {},
-		["core.integrations.telescope"] = {},
-	},
+        ["core.presenter"] = {
+            config = {
+                zen_mode = "zen-mode",
+            },
+        },
+        ["core.norg.completion"] = {
+            config = {
+                engine = "nvim-cmp",
+            },
+        },
+        ["core.norg.concealer"] = {},
+        ["core.norg.dirman"] = {
+            config = {
+                workspaces = {
+                    home = "~/notes/home",
+                    work = "~/notes/work",
+                },
+            },
+        },
+        ["core.norg.qol.toc"] = {},
+        ["core.integrations.telescope"] = {},
+    },
 })
