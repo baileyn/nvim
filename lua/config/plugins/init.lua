e48135d5f8145742adf35b442a61e35b721ccdbc
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-08-08 11:03:24 -0600

    feat(plugins): add plenary dependency

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -334,6 +334,7 @@ local function init()
     })
     use({
         "jose-elias-alvarez/null-ls.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
         config = function()
             require("null-ls").setup({
                 sources = {
