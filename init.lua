bac0f40de90995d718845ff1d5b3698fa86374a4
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-10-08 19:42:50 -0600

    feat(plugins): add lsp keymaps

--- init.lua
+++ init.lua
@@ -60,12 +60,39 @@ return require('packer').startup(function(use)
         'neovim/nvim-lspconfig',
         after = { 'nvim-cmp', 'mason-lspconfig.nvim' },
         config = function()
+            -- Setup keymaps
+            local opts = { noremap=true, silent=true }
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
+            local on_attach = function(client, bufnr)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                -- Mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local bufopts = { noremap=true, silent=true, buffer=bufnr }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+                vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+            end
+
             local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
  
             require('mason-lspconfig').setup_handlers {
                 function(server_name)
                     require('lspconfig')[server_name].setup {
-                        capabilities = capabilities
+                        capabilities = capabilities,
+                        on_attach = on_attach
                     }
                 end,
             }
@@ -142,8 +169,8 @@ return require('packer').startup(function(use)
                 }),
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
-                    { name = 'vsnip' }, -- For vsnip users.
-                    -- { name = 'luasnip' }, -- For luasnip users.
+                    -- { name = 'vsnip' }, -- For vsnip users.
+                    { name = 'luasnip' }, -- For luasnip users.
                     -- { name = 'ultisnips' }, -- For ultisnips users.
                     -- { name = 'snippy' }, -- For snippy users.
                 }, {
@@ -201,6 +228,27 @@ return require('packer').startup(function(use)
         config = function()
             require('mason-lspconfig').setup {
                 automatic_installation = true
+            }
+        end
+    }
+
+    use {
+        'nvim-neorg/neorg',
+        requires = 'nvim-lua/plenary.nvim',
+        run = ":Neorg sync-parsers",
+        config = function()
+            require('neorg').setup {
+                load = {
+                    ['core.defaults'] = {},
+                    ['core.norg.dirman'] = {
+                        config = {
+                            workspaces = {
+                                work = '~/notes/work',
+                                home = '~/notes/home',
+                            }
+                        }
+                    }
+                }
             }
         end
     }
