aae132ca8b3cf61a402e5d653ffc9c3ddcf057b1
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2023-02-01 14:35:58 -0700

    fix: tabs

--- init.lua
+++ init.lua
@@ -208,8 +208,6 @@ return require('packer').startup(function(use)
                     ['<Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
-                        elseif vim.fn['vsnip#available'](1) == 1 then
-                            feedkey('<Plug>(vsnip-expand-or-jump)', '')
                         elseif has_words_before() then
                             cmp.complete()
                         else
@@ -220,8 +218,8 @@ return require('packer').startup(function(use)
                     ['<S-Tab>'] = cmp.mapping(function()
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-                            feedkey('<Plug>(vsnip-jump-prev)', '')
+                        else
+                            fallback()
                         end
                     end, { 'i', 's' }),
                 },
