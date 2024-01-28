b2889ace4bf94667a607d3df5082a0a87fa4e9c4
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2024-01-27 22:35:27 -0700

    feat(lsp): add commin actions

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -14,6 +14,9 @@ return {
         tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            require('telescope').setup({
+            })
+
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>fF', builtin.find_files, {})
             vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
