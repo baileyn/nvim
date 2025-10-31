4220b56dbfb318f775608399967e0764ac24d57e
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2025-10-31 14:16:37 -0400

    cleanup: fix some deprecation warnings

--- init.lua
+++ init.lua
@@ -80,8 +80,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set('n', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,23 @@
 return {
-    {
-        'github/copilot.vim',
-        config = function()
-            vim.keymap.set('i', '<C-y>', 'copilot#Accept("")', {
-                expr = true,
-                replace_keycodes = false,
-            })
-            vim.g.copilot_no_tab_map = true
-        end,
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.keymap.set('i', '<C-y>', 'copilot#Accept("")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
-    {
-        'CopilotC-Nvim/CopilotChat.nvim',
-        branch = 'canary',
-        dependencies = {
-            { 'github/copilot.vim' },    -- or github/copilot.vim
-            { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-        },
-        opts = {
-            debug = true, -- Enable debugging
-            -- See Configuration section for rest
-        },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
     },
+  },
 }
